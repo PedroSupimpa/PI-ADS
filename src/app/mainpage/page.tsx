9aeb25162ff9bda57453f37e44ad32a23efe18d6
Pedro Souza <shiro2303@gmail.com>
2023-08-05 13:31:27 -0300

    Trying new component

--- src/app/mainpage/page.tsx
+++ src/app/mainpage/page.tsx
@@ -1,14 +1,24 @@
 "use client";
-import SimpleAnchorCard from "@/components/SimpleAnchorCard/page";
+import ImageTextCard from "@/components/ImageTextCard/page";
 
 export default function MainPage() {
   return (
     <>
-      <SimpleAnchorCard
-        title="Zezin"
-        subtitle="O corno"
+      <ImageTextCard
+        title="Zezin, o corno"
+        subtitle="o maior corno do mundo"
         image="https://media.sketchfab.com/models/243f544627214afeb17535ce53ca75fa/thumbnails/220b50db33464ae5aa902a30704bb92f/1024x576.jpeg"
-      ></SimpleAnchorCard>
+      ></ImageTextCard>
+      <ImageTextCard
+        title="Joaozin, o corno"
+        subtitle="o maior corno do mundo"
+        image="https://media.sketchfab.com/models/243f544627214afeb17535ce53ca75fa/thumbnails/220b50db33464ae5aa902a30704bb92f/1024x576.jpeg"
+      ></ImageTextCard>
+      <ImageTextCard
+        title="Josezin, o corno"
+        subtitle="o maior corno do mundo"
+        image="https://media.sketchfab.com/models/243f544627214afeb17535ce53ca75fa/thumbnails/220b50db33464ae5aa902a30704bb92f/1024x576.jpeg"
+      ></ImageTextCard>
     </>
   );
 }
