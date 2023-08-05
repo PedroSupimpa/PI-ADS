d40156168b947f0b82f9fb119476dc4c109d09e5
Pedro Souza <shiro2303@gmail.com>
2023-08-05 17:30:30 -0300

    adding some fake data for tests

---
+++ src/data.jsx
@@ -0,0 +1,38 @@
+export const testData = [
+  {
+    title: "Zezin, o corno",
+    subtitle: "o maior corno do mundo",
+    image:
+      "https://media.sketchfab.com/models/243f544627214afeb17535ce53ca75fa/thumbnails/220b50db33464ae5aa902a30704bb92f/1024x576.jpeg",
+  },
+  {
+    title: "Jukinha, o burro",
+    subtitle: "King of the burros",
+    image:
+      "https://media.sketchfab.com/models/243f544627214afeb17535ce53ca75fa/thumbnails/220b50db33464ae5aa902a30704bb92f/1024x576.jpeg",
+  },
+  {
+    title: "Jukinha, o corno",
+    subtitle: "o maior corno do mundo",
+    image:
+      "https://play-lh.googleusercontent.com/TdByGSl0ydL445smYzXVKWW-7E5G_jG0MJAti9DlevS1unPdaAJmzETUZ2F7-PZSovws",
+  },
+  {
+    title: "Minion",
+    subtitle: "Sapecado de ADCarry",
+    image:
+      "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blt071b481a32fcd0b0/5fc84b2f3cdbef7187cec597/Doomed_Minion_profileicon.png",
+  },
+  {
+    title: "Jukinha, o corno",
+    subtitle: "o maior corno do mundo",
+    image:
+      "https://static.wikia.nocookie.net/logopedia/images/d/db/Fortnite_S1.svg/revision/latest?cb=20210330161743.jpeg",
+  },
+  {
+    title: "Jukinha, o corno",
+    subtitle: "o maior corno do mundo",
+    image:
+      "https://play-lh.googleusercontent.com/TdByGSl0ydL445smYzXVKWW-7E5G_jG0MJAti9DlevS1unPdaAJmzETUZ2F7-PZSovws",
+  },
+];
