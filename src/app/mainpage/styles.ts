41d91040936542e1a705ac6ac752309f3d4faeb2
Pedro Souza <shiro2303@gmail.com>
2023-08-05 17:06:20 -0300

    added carousel style

--- src/app/mainpage/styles.ts
+++ src/app/mainpage/styles.ts
@@ -58,3 +58,22 @@ export const Text = styled.div`
   font-family: "Roboto", sans-serif;
   padding: 0.2vh;
 `;
+
+export const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1300, min: 464 },
+    items: 4,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
