ca2075113856b471b0738cbd72517a2946f2c67e
Pedro Souza <shiro2303@gmail.com>
2023-08-05 18:38:28 -0300

    adjust css

--- src/components/ImageTextCard/styles.ts
+++ src/components/ImageTextCard/styles.ts
@@ -10,12 +10,13 @@ export const Container = styled.div`
   align-items: center;
   padding: 1em;
   margin: 1em;
-  gap: 1vw;
+
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
 
   @media (max-width: 464px) {
     width: 80%;
-    height: 100%;
+    gap: 2vw;
+    height: 50%;
   }
 `;
 
@@ -25,6 +26,7 @@ export const TextContainer = styled.div`
   width: 50%;
   height: 100%;
   align-items: center;
+  margin-left: 1vw;
 `;
 
 export const Title = styled.div`
@@ -37,9 +39,8 @@ export const Title = styled.div`
   width: 100%;
   height: 100%;
   box-sizing: border-box;
-  white-space: nowrap; /* Prevent title from wrapping to a new line */
-  text-overflow: ellipsis; /* Add ellipsis (...) when text overflows */
-  overflow: hidden; /* Hide any overflowing text beyond the container */
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 export const Subtitle = styled.div`
@@ -50,14 +51,13 @@ export const Subtitle = styled.div`
   width: 100%;
   height: 100%;
   box-sizing: border-box;
-  white-space: nowrap; /* Prevent title from wrapping to a new line */
-  text-overflow: ellipsis; /* Add ellipsis (...) when text overflows */
-  overflow: hidden; /* Hide any overflowing text beyond the container */
+  white-space: nowrap;
+  overflow: hidden;
 `;
 
 export const ImageContainer = styled.div`
   min-height: 30px;
-  min-width: 70px;
+  min-width: 90px;
   width: 6vw;
   height: 10vh;
 `;
