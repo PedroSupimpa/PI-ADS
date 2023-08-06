b1e45665d94508551438a815f353d101768f8f67
Pedro Souza <shiro2303@gmail.com>
2023-08-06 18:20:47 -0300

    Creating Nav Component

---
+++ src/components/nav/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 6vh;
+  background: white;
+  border-bottom: 1px solid #f2f2f2;
+`;
+
+export const SeachContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 30vw;
+  height: 80%;
+`;
+
+export const IconContainer = styled.div`
+  margin: 1vw;
+`;
