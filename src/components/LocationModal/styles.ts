cc082ff2519611eed88de8768e92ce9e71639e63
Pedro Souza <shiro2303@gmail.com>
2023-09-16 19:57:42 -0300

    fixing locationValue

--- src/components/LocationModal/styles.ts
+++ src/components/LocationModal/styles.ts
@@ -4,9 +4,13 @@ export const ContainerLocation = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  width: 100%;
 `;
 
 export const LocationValue = styled.div`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
   @media (max-width: 464px) {
     display: none;
     justify-content: center;
