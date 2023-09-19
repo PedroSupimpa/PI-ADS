cfa9fddc791516690d49867e17530151b929b460
Pedro Souza <shiro2303@gmail.com>
2023-09-19 00:44:27 -0300

    adding footer mock text

--- src/components/Footer/index,.tsx
+++ src/components/Footer/index,.tsx
@@ -1,9 +1,31 @@
-import { FooterContainer } from "./styles";
+import {
+  BrandLogo,
+  CopyrigthText,
+  FooterBottom,
+  FooterContainer,
+  LinkOptions,
+  LinksTerms,
+} from "./styles";
 
 const Footer = () => {
   return (
     <>
-      <FooterContainer></FooterContainer>
+      <FooterContainer>
+        <FooterBottom>
+          <BrandLogo />
+          <CopyrigthText>
+            {`© Copyright 2023 - All Ondemand - Todos os direitos reservados All Ondemand com Agência de Restaurantes Online S.A.
+
+CNPJ 00.000.000/0000-00 /  Taguatinga, Brasilia/DF - CEP 00.000-000`}
+          </CopyrigthText>
+          <LinksTerms>
+            <LinkOptions>{"Termos e condições de uso"}</LinkOptions>
+            <LinkOptions>{"Código de conduta"}</LinkOptions>
+            <LinkOptions>{"Privacidade"}</LinkOptions>
+            <LinkOptions>{"Dicas de segurança"}</LinkOptions>
+          </LinksTerms>
+        </FooterBottom>
+      </FooterContainer>
     </>
   );
 };
